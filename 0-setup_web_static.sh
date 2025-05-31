770fb5f8ab7c97981865e07c666700458c2f40fa
evand3rr <e.manna@alustudent.com>
2025-05-31 15:58:36 +0200

    Updated 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,14 +4,11 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-# Create necessary directories
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-# Add test HTML file to the test folder
 sudo touch /data/web_static/releases/test/index.html
 sudo echo "<html>
   <head>
@@ -21,18 +18,10 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a symbolic link to /data/web_static/current
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Test the Nginx configuration to ensure there are no syntax errors
-sudo nginx -t
-
-# Restart Nginx to apply the changes
 sudo service nginx restart
-
